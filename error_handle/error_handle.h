69b93466cf0affa6bfd37cf1ddc54e1f4c55eda0
gyujun9403 <gujun112@naver.com>
2022-07-16 18:43:49 +0900

    make other sub

--- error_handle/error_handle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <stdio.h>
-
-void err_quit(const char* msg);
-void err_display(const char* msg);
-void err_quitW(const wchar_t* msg);
-void err_displayW(const wchar_t* msg);
